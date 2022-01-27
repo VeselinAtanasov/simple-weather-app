97192ea122041f9650938df44d5142f8d9898977
VeselinAtanasov <veselin_atanasov88@abv.bg>
2022-01-27 14:56:16 +0200

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/getForecast');
 // const extractForecast = require('./utils/extractForecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -95,7 +96,7 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
 })
 ;
